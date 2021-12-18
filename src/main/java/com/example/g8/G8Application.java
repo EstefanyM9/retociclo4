07d2541f6616541ed5d4ff03176d64feded34d0f
EstefanyM9 <91507176+EstefanyM9@users.noreply.github.com>
2021-12-18 09:00:22 -0500

    change application file

--- src/main/java/com/example/g8/G8Application.java
+++ src/main/java/com/example/g8/G8Application.java
@@ -1,13 +1,35 @@
 package com.example.g8;
 
+import com.example.g8.repositories.ClotheRepository;
+import com.example.g8.repositories.OrderRepository;
+import com.example.g8.repositories.UserRepository;
+import com.example.g8.repositoriesCrud.ClotheCrudRepository;
+import com.example.g8.repositoriesCrud.OrderCrudRepository;
+import com.example.g8.repositoriesCrud.UserCrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class G8Application {
+public class G8Application implements CommandLineRunner {
 
-	public static void main(String[] args) {
+    @Autowired	
+    private ClotheCrudRepository clotheRepository;
+    @Autowired	
+    private UserCrudRepository userRepository;
+    @Autowired	
+    private OrderCrudRepository orderRepository;
+                
+    public static void main(String[] args) {
 		SpringApplication.run(G8Application.class, args);
 	}
+
+    @Override
+    public void run(String... args) throws Exception {
+      clotheRepository.deleteAll();
+      userRepository.deleteAll();
+      orderRepository.deleteAll();
+    }
 
 }
